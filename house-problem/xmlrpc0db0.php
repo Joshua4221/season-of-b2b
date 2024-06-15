c6a9a64036dda7c00616fdbbdb8adae82bcc7d2c
zeals <zealousemmy12@gmail.com>
2024-06-15 11:21:02 +0100

    sds

---
+++ house-problem/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://truelysell-wp.dreamstechnologies.com/house-problem</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://truelysell-wp.dreamstechnologies.com/house-problem/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://truelysell-wp.dreamstechnologies.com/house-problem/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://truelysell-wp.dreamstechnologies.com/house-problem/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://truelysell-wp.dreamstechnologies.com/house-problem/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://truelysell-wp.dreamstechnologies.com/house-problem/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
